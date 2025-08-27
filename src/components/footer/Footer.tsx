223376da515e38acbbb56cee0aba266360d20b4a
cosmos <alfredchrisayo@gmail.com>
2025-08-27 11:43:38 +0000

    worked on the new-footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -45,125 +45,140 @@ const Footer = () => {
 
   return (
     <>
-      <footer className="h-[60rem] bg-[#FF7700] relative flex justify-center items-center flex-col ">
-        <div className="absolute w-12/13  justify-self-center h-96 -top-70 woman-bg flex flex-col gap-5 rounded-2xl place-content-center items-center">
-          <h1 className="text-5xl font-extrabold text-white font-sans">
-            Ready to shape your future?{" "}
-          </h1>
-
-          <h1 className="text-5xl font-extrabold text-white font-sans">
-            Let's begin!
-          </h1>
-
-          <button className="bg-[#EF760C63] rounded-full h-16  text-2xl font-sans font-normal text-white p-2 w-[270px]">
-            Get Started
-          </button>
-        </div>
-
-        <div className="w-full h-auto flex justify-between  ">
-          <div className="footer-img-container flex justify-start flex-col w-1/4 h-auto px-8 ">
-            <div className="w-5/6 px-10 ">
+      <footer className="h-[90rem]   relative flex justify-center items-center flex-col ">
+        <div className="h-2/5  w-full relative bg-white">
+          <div className="w-full h-1/2 flex justify-center items-center ">
+            <span>
+              {" "}
               <img
                 src={logo}
                 alt=""
-                className="w-full h-44 object-contain -ml-12"
+                className="w-72 h-60 object-contain"
               />{" "}
-              <p className="font-sans  font-medium text-white">
-                We believe education should be accessible, inclusive, and
-                globally enriching. Whether you're a student dreaming of
-                studying abroad or an institution striving for a more inclusive
-                international student experience, Etoeko is here to help you
-                thrive.
-              </p>
-            </div>
-          </div>
-
-          <div className="footer-img-container  w-1/4 h-auto p-14 ">
-            <h1 className="text-xl font-extrabold font-sans text-white">
-              {subMenus[0].header}
-            </h1>
-            <ul className="flex flex-col gap-10 mt-10  ">
-              {subMenus[0].children.map((link) => (
-                <li className="cursor-pointer text-white">{link}</li>
-              ))}
-            </ul>
-          </div>
-          <div className="footer-img-container  w-1/4 h-auto  p-10">
-            <h1 className="text-xl font-extrabold font-sans text-white">
-              {subMenus[1].header}
-            </h1>
-            <ul className="flex flex-col gap-10 mt-10  ">
-              {subMenus[1].children.map((link) => (
-                <li className="cursor-pointer text-white">{link}</li>
-              ))}
-            </ul>
-          </div>
-          <div className="footer-img-container  w-1/4 h-auto  p-10">
-            <h1 className="text-xl font-extrabold font-sans text-white">
-              {subMenus[2].header}
-            </h1>
-            <ul className="flex flex-col gap-10 mt-10  ">
-              {subMenus[2].children.map((link) => (
-                <li className="cursor-pointer text-white">{link}</li>
-              ))}
-            </ul>
+            </span>
           </div>
         </div>
 
-        <div className=" w-full h-auto flex justify-between px-16  ">
-          <div className="w-[246px] h-[218px] ">
-            <img
-              src={glassHouse}
-              alt="complex-building"
-              className="w-full h-full object-cover"
-            />
+        <div className="h-full w-full bg-[#FF7700] flex justify-center flex-col items-center relative ">
+          <div className="absolute w-12/13  justify-self-center h-96 -top-56 woman-bg flex flex-col gap-5 rounded-2xl place-content-center items-center">
+            <h1 className="text-5xl font-extrabold text-white font-sans">
+              Ready to shape your future?{" "}
+            </h1>
+
+            <h1 className="text-5xl font-extrabold text-white font-sans">
+              Let's begin!
+            </h1>
+
+            <button className="bg-[#EF760C63] rounded-full h-16  text-2xl font-sans font-normal text-white p-2 w-[270px]">
+              Get Started
+            </button>
           </div>
 
-          <div className="w-1/4  flex justify-center items-end px-10 py-4">
-            <div className=" w-full flex  items-end justify-center gap-4 ">
-              <FaLocationDot className="text-6xl text-white" />
-              <p className="text-white font-sans">
-                6th Floor, Park Royal, 2 Lake Drive, First Central 200 Abuja,
-                NW10 7FQ, Nigeria.{" "}
-              </p>
+          <div className="relative h-auto  flex justify-evenly w-full px-8  items-start mt-40">
+            <div className="footer-img-container flex justify-start flex-col w-1/4 h-auto px-8  ">
+              <div className="w-5/6 px-10 -mt-5 ">
+                <img
+                  src={logo}
+                  alt=""
+                  className="w-full h-44 object-contain -ml-12"
+                />{" "}
+                <p className="font-sans  font-medium text-white">
+                  We believe education should be accessible, inclusive, and
+                  globally enriching. Whether you're a student dreaming of
+                  studying abroad or an institution striving for a more
+                  inclusive international student experience, Etoeko is here to
+                  help you thrive.
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="w-1/4  flex justify-center items-end px-10 py-4">
-            <div className=" w-full flex  items-center justify-center gap-4 ">
-              <HiMailOpen className="text-6xl text-white" />
-              <p className="text-white font-sans">info@etoeko.com.uk</p>
+
+            <div className="footer-img-container  w-1/4 h-auto p-10 ">
+              <h1 className="text-xl font-extrabold font-sans text-white">
+                {subMenus[0].header}
+              </h1>
+              <ul className="flex flex-col gap-10 mt-10  ">
+                {subMenus[0].children.map((link) => (
+                  <li className="cursor-pointer text-white">{link}</li>
+                ))}
+              </ul>
+            </div>
+            <div className="footer-img-container  w-1/4 h-auto  p-10">
+              <h1 className="text-xl font-extrabold font-sans text-white">
+                {subMenus[1].header}
+              </h1>
+              <ul className="flex flex-col gap-10 mt-10  ">
+                {subMenus[1].children.map((link) => (
+                  <li className="cursor-pointer text-white">{link}</li>
+                ))}
+              </ul>
+            </div>
+            <div className="footer-img-container  w-1/4 h-auto  p-10">
+              <h1 className="text-xl font-extrabold font-sans text-white">
+                {subMenus[2].header}
+              </h1>
+              <ul className="flex flex-col gap-10 mt-10  ">
+                {subMenus[2].children.map((link) => (
+                  <li className="cursor-pointer text-white">{link}</li>
+                ))}
+              </ul>
             </div>
           </div>
 
-          <div className="w-1/4  flex justify-center items-end px-10 py-4">
-            <div className=" w-full flex  items-center justify-start gap-10 ">
-              <FaFacebook className="text-3xl text-white" />
-              <FaLinkedin className="text-3xl text-white" />
-              <FaInstagram className="text-3xl text-white" />
-              <RiTwitterXFill className="text-3xl text-white" />
-              <FaYoutube className="text-3xl text-white" />
+          <div className="relative h-1/3  flex justify-evenly w-full px-8 items-end ">
+            <div className="w-[246px] h-[218px] ">
+              <img
+                src={glassHouse}
+                alt="complex-building"
+                className="w-full h-full object-cover"
+              />
+            </div>
+
+            <div className="w-1/4  flex justify-center items-end px-10 py-4">
+              <div className=" w-full flex  items-end justify-center gap-4 ">
+                <FaLocationDot className="text-6xl text-white" />
+                <p className="text-white font-sans">
+                  6th Floor, Park Royal, 2 Lake Drive, First Central 200 Abuja,
+                  NW10 7FQ, Nigeria.{" "}
+                </p>
+              </div>
+            </div>
+            <div className="w-1/4  flex justify-center items-end px-10 py-4">
+              <div className=" w-full flex  items-center justify-center gap-4 ">
+                <HiMailOpen className="text-6xl text-white" />
+                <p className="text-white font-sans">info@etoeko.com.uk</p>
+              </div>
+            </div>
+
+            <div className="w-1/4  flex justify-center items-end px-10 py-4">
+              <div className=" w-full flex  items-center justify-start gap-10 ">
+                <FaFacebook className="text-3xl text-white" />
+                <FaLinkedin className="text-3xl text-white" />
+                <FaInstagram className="text-3xl text-white" />
+                <RiTwitterXFill className="text-3xl text-white" />
+                <FaYoutube className="text-3xl text-white" />
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className=" w-full  mt-5 px-16">
-          <div className="w-full border-b border-white"></div>
-          <div className="h-auto  flex justify-between items-center gap-2 mt-5">
-            <div className=" w-2/6  font-sans flex justify-center items-center text-white">
-              <p>
-                Copyright &copy; {new Date().getFullYear()} Consulting
-                Education, All rights reserved. Powered by Etoeko.
-              </p>
-            </div>
-            <div className=" w-4/6  p-2">
-              <div className="flex justify-end items-center gap-10">
-                {["Terms of use", "Cookie policy", "Privacy policy"].map(
-                  (text) => (
-                    <Link to={"#"} className="font-sans text-xl text-white">
-                      {text}
-                    </Link>
-                  )
-                )}
+          <div className=" w-full  bg-[#FF7700] mt-10  px-16">
+            <div className="w-full border-b border-white"></div>
+            <div className="h-auto  flex justify-between items-center gap-2 mt-5">
+              <div className=" w-2/6  font-sans flex justify-center items-center text-white">
+                <p>
+                  Copyright &copy; {new Date().getFullYear()} Consulting
+                  Education, All rights reserved. Powered by Etoeko.
+                </p>
+              </div>
+              <div className=" w-4/6  p-2">
+                <div className="flex justify-end items-center gap-10">
+                  {["Terms of use", "Cookie policy", "Privacy policy"].map(
+                    (text) => (
+                      <Link to={"#"} className="font-sans text-xl text-white">
+                        {text}
+                      </Link>
+                    )
+                  )}
+                </div>
               </div>
             </div>
           </div>
--- src/pages/Home/sections/our-partners/OurPartners.tsx
+++ src/pages/Home/sections/our-partners/OurPartners.tsx
@@ -2,9 +2,9 @@
 const OurPartners = () => {
   return (
     <>
-      <div className="h-screen flex py-3 items-center relative flex-col mt-4 mb-4 ">
+      <div className="h-auto flex py-3 items-center relative flex-col mt-4 mb-4 ">
         <h1 className="text-4xl font-sans ">Our Partners</h1>
-        {/* <div className="border w-full h-44 mt-10"></div> */}
+        <div className="border w-full h-44 mt-10"></div>
       </div>
     </>
   );
